da67334e5d94744061c725ac82911cbe6ae17df8
kiweeherman <137016035+kiweeherman@users.noreply.github.com>
2023-07-01 18:03:04 -0500

    Complete Rewrite
    Complete rewrite for HTML and CSS, and restart on JS

--- src/script.js
+++ src/script.js
@@ -1,38 +1,37 @@
-// This section will now be working to change the name of the City at the top of the page when the user submits a city.
+// Between here and the next note, the code works to display weekday and time in 12 hr. format with Am/Pm
+let currentDay = document.getElementById("date-and-time");
 
-let cityName = document.getElementById("city-name");
-let searchButton = document.getElementById("city-submit");
-let searchField = document.getElementById("city-search");
-
-searchButton.addEventListener("click", searchForCity);
-
-function toTitleCase(str) {
-  return str
-    .toLowerCase()
-    .split(" ")
-    .map(function (word) {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    })
-    .join(" ");
-}
-
-function searchForCity(event) {
-  event.preventDefault();
-  let apiKey = "af198b69f06145dba9541337231206";
-  let cityTerm = searchField.value;
-  let apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityTerm}&aqi=no`;
-  if (cityTerm === "") {
-    cityName.innerHTML = "Lisbon";
+let now = new Date();
+let weekdays = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+function amPm() {
+  if (now.getHours < 12) {
+    return "AM";
   } else {
-    cityName.innerHTML = toTitleCase(cityTerm);
+    return "PM";
   }
-  return axios.get(apiUrl).then(produceTemp).catch(handleError);
 }
+let hours = now.getHours() % 12;
+if (hours === 0) {
+  hours = 12;
+}
+let mins = now.getMinutes();
+if (mins < 10) {
+  mins = `0${now.getMinutes()}`;
+}
+let currentTime = `${hours}:${mins}`;
+let currentWeekDay = weekdays[now.getDay()];
+console.log(now.getHours());
+currentDay.innerHTML = `${currentWeekDay}, ${currentTime} ${amPm()}`;
 
-function produceTemp(response) {
-  let changeTemp = document.getElementById("city-temp");
-  changeTemp.innerHTML = `${response.data.current.temp_f} ° F`;
+// From here, the code will work to display the User's city when searched and submitted
+let userCity = document.getElementById("city-name");
+let search = document.getElementById("search-city");
+let searchButton = document.getElementById("button-addon2");
+
+searchButton.addEventListener("click", displayCity);
+
+function displayCity(response) {
+  response.preventDefault();
+  userCity.innerHTML = search.value;
 }
-function handleError(error) {
-  console.log(`Error: ${error}`);
-}
+// For now, this will do while I work on the API integration
