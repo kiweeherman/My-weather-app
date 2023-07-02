019a2aa22a67381aa5d82e64faefdf1d2aed1244
kiweeherman <137016035+kiweeherman@users.noreply.github.com>
2023-07-02 12:47:03 -0500

    Added forecast to weather app

--- src/script.js
+++ src/script.js
@@ -22,6 +22,14 @@ let currentTime = `${hours}:${mins}`;
 let currentWeekDay = weekdays[now.getDay()];
 currentDay.innerHTML = `${currentWeekDay}, ${currentTime} ${amPm()}`;
 
+//This section will automatically update the forecast days to the correct days of the week
+document.getElementById("forecast-1").innerHTML = weekdays[now.getDay() + 1];
+document.getElementById("forecast-2").innerHTML = weekdays[now.getDay() + 2];
+document.getElementById("forecast-3").innerHTML = weekdays[now.getDay() + 3];
+document.getElementById("forecast-4").innerHTML = weekdays[now.getDay() + 4];
+document.getElementById("forecast-5").innerHTML = weekdays[now.getDay() + 5];
+document.getElementById("forecast-6").innerHTML = weekdays[now.getDay() + 6];
+
 // From here, the code will work to display the User's city when searched and submitted
 let userCity = document.getElementById("city-name");
 let search = document.getElementById("search-city");
@@ -38,6 +46,8 @@ function searchCity(response) {
   let unit = "metric";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=${unit}`;
   axios.get(apiUrl).then(displayCity);
+  let forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${key}&units=${unit}`;
+  axios.get(forecastUrl).then(displayForecastC);
 }
 function displayCity(data) {
   temp.innerHTML = `${data.data.temperature.current} °C`;
@@ -46,10 +56,64 @@ function displayCity(data) {
   let windspeed = data.data.wind.speed;
   forecast.innerHTML = `Mr. Frog says today is a ${statement} day! <br> He also notes windspeeds of ${windspeed} kmph`;
   console.log(data.data);
-
   let frogBoy = document.getElementById("frog-boy");
   frogBoy.src = `img/${data.data.condition.icon}.PNG`;
 }
+
+function displayForecastC(data) {
+  console.log(data);
+  // Day 1
+  document.getElementById(
+    "temp-1"
+  ).innerHTML = `${data.data.daily[0].temperature.day}° C`;
+  // Day 2
+  document.getElementById(
+    "temp-2"
+  ).innerHTML = `${data.data.daily[1].temperature.day}° C`;
+  //Day 3
+  document.getElementById(
+    "temp-3"
+  ).innerHTML = `${data.data.daily[2].temperature.day}° C`;
+  //Day 4
+  document.getElementById(
+    "temp-4"
+  ).innerHTML = `${data.data.daily[3].temperature.day}° C`;
+  //Day 5
+  document.getElementById(
+    "temp-5"
+  ).innerHTML = `${data.data.daily[4].temperature.day}° C`;
+  //And Day 6
+  document.getElementById(
+    "temp-6"
+  ).innerHTML = `${data.data.daily[5].temperature.day}° C`;
+  // And now the function to change all of the images to match the forecast
+
+  // Day 1
+  document.getElementById(
+    "img-1"
+  ).src = `${data.data.daily[0].condition.icon_url}`;
+  // Day 2
+  document.getElementById(
+    "img-2"
+  ).src = `${data.data.daily[1].condition.icon_url}`;
+  //Day 3
+  document.getElementById(
+    "img-3"
+  ).src = `${data.data.daily[2].condition.icon_url}`;
+  //Day 4
+  document.getElementById(
+    "img-4"
+  ).src = `${data.data.daily[3].condition.icon_url}`;
+  //Day 5
+  document.getElementById(
+    "img-5"
+  ).src = `${data.data.daily[4].condition.icon_url}`;
+  //And Day 6
+  document.getElementById(
+    "img-6"
+  ).src = `${data.data.daily[5].condition.icon_url}`;
+}
+
 // Now let's figure out what we can do about finding the current location
 let locator = document.getElementById("current-location");
 locator.addEventListener("click", onSuccess);
@@ -69,9 +133,13 @@ function geolocator(data) {
   let statement = data.data.condition.description;
   let windspeed = data.data.wind.speed;
   forecast.innerHTML = `Mr. Frog says today is a ${statement} day! <br> He also notes windspeeds of ${windspeed}kmph`;
-
   let frogBoy = document.getElementById("frog-boy");
   frogBoy.src = `img/${data.data.condition.icon}.PNG`;
+  let city = data.data.city;
+  let key = "3d95dc3f33f458odab21943t86b91c04";
+  let unit = metric;
+  let forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${key}&units=${unit}`;
+  axios.get(forecastUrl).then(displayForecastC);
 }
 
 // Word, now that that is done, we need to integrate the unit conversion between metric and imperial
@@ -84,6 +152,10 @@ function convertToC() {
   let key = "3d95dc3f33f458odab21943t86b91c04";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${userCity.innerHTML}&key=${key}&units=metric`;
   axios.get(apiUrl).then(displayMetric);
+  let apikey = "3d95dc3f33f458odab21943t86b91c04";
+  let unit = metric;
+  let forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${userCity.innerHTML}&key=${apikey}&units=${unit}`;
+  axios.get(forecastUrl).then(displayForecastC);
 }
 function displayMetric(data) {
   temp.innerHTML = `${data.data.temperature.current} °C`;
@@ -104,6 +176,9 @@ function convertToF() {
   let key = "3d95dc3f33f458odab21943t86b91c04";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${userCity.innerHTML}&key=${key}&units=imperial`;
   axios.get(apiUrl).then(displayImperial);
+  let apikey = "3d95dc3f33f458odab21943t86b91c04";
+  let forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${userCity.innerHTML}&key=${apikey}&units=imperial`;
+  axios.get(forecastUrl).then(displayForecastF);
 }
 function displayImperial(data) {
   temp.innerHTML = `${data.data.temperature.current} °F`;
@@ -111,8 +186,34 @@ function displayImperial(data) {
   let statement = data.data.condition.description;
   let windspeed = data.data.wind.speed;
   forecast.innerHTML = `Mr. Frog says today is a ${statement} day! <br> He also notes windspeeds of ${windspeed} mph`;
-  console.log(data.data);
+}
 
-  let frogBoy = document.getElementById("frog-boy");
-  frogBoy.src = `img/${data.data.condition.icon}.PNG`;
+// This will be the function "displayForecastF", which is a lot of annoying code. So I decided to leave it down here
+
+function displayForecastF(data) {
+  console.log(data);
+  // Day 1
+  document.getElementById(
+    "temp-1"
+  ).innerHTML = `${data.data.daily[0].temperature.day}° F`;
+  // Day 2
+  document.getElementById(
+    "temp-2"
+  ).innerHTML = `${data.data.daily[1].temperature.day}° F`;
+  //Day 3
+  document.getElementById(
+    "temp-3"
+  ).innerHTML = `${data.data.daily[2].temperature.day}° F`;
+  //Day 4
+  document.getElementById(
+    "temp-4"
+  ).innerHTML = `${data.data.daily[3].temperature.day}° F`;
+  //Day 5
+  document.getElementById(
+    "temp-5"
+  ).innerHTML = `${data.data.daily[4].temperature.day}° F`;
+  //And Day 6
+  document.getElementById(
+    "temp-6"
+  ).innerHTML = `${data.data.daily[5].temperature.day}° F`;
 }
